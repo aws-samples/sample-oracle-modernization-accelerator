0fe242501906fe74f59e19eba3eccdf0712830b0
Changik Lee <changik@amazon.com>
2025-07-10 19:15:25 +0900

    Clean up - config dir

--- config/find_trc_location.sql
+++
@@ -1,9 +0,0 @@
--- TRC 파일 위치 확인
-SELECT name, value 
-FROM v$parameter 
-WHERE name IN ('user_dump_dest', 'diagnostic_dest', 'background_dump_dest');
-
--- 현재 세션의 TRC 파일명 확인
-SELECT value 
-FROM v$diag_info 
-WHERE name = 'Default Trace File';
